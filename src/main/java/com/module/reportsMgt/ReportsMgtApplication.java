de1dc0f08e981c1a839978beaad4405c9f4da1c9
Ilnur Nasibullin <softilnyr16@gmail.com>
2019-04-18 12:26:07 +0300

    Initial

---
+++ src/main/java/com/module/reportsMgt/ReportsMgtApplication.java
@@ -0,0 +1,13 @@
+package com.module.reportsMgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsMgtApplication.class, args);
+	}
+
+}
