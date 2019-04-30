bc23d7cc30de47b27475f48111b6207869d158dd
nethmihetti <nethmihettiarachchi484@gmail.com>
2019-04-30 14:55:30 +0300

    dev-ops1

--- infrastructure/commands.sh
+++ infrastructure/commands.sh
@@ -6,6 +6,6 @@ cd innoReports
 
 docker build --no-cache -f infrastructure/Dockerfile -t innoreports .
 
-docker run -e -d CLASSIFICATION_SERVICE_URL=http://10.90.138.222:8081 \
+docker run -d -e CLASSIFICATION_SERVICE_URL=http://10.90.138.222:8081 \
 -e PERSISTENCE_SERVICE_URL=http://10.90.138.222:5000 \
 -p 8080:8080 innoreports:latest
